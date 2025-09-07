a70ad973f7fc72f51e0e35ac4b45db9b8e6d6ff1
AntonAshmanov <antosha.v.ashmanov@gmail.com>
2025-09-07 15:14:26 +0300

    add main elements for the matrix algebra

---
+++ src/matrix_algebra.h
@@ -0,0 +1,38 @@
+#ifndef MATRIX_ALGEBRA_H
+#define MATRIX_ALGEBRA_H
+
+template<class O> 
+struct math_object_base { 
+    O& self(){ 
+        return static_cast<O&>(*this); 
+    } 
+    const O& self() const { 
+        return static_cast<const O&>(*this); 
+    } 
+};
+
+template<class E> 
+struct expression : math_object_base<E>{}; 
+
+struct add{};
+struct multi{};
+
+template<class E1, class OP, class E2> 
+struct binary_expression : 
+expression<binary_expression<E1, OP, E2> > 
+{ 
+    binary_expression(const expression<E1> &expr1, 
+                        const OP &op, const expression<E2> &expr2) : 
+                    expr1(expr1.self()), op(op), expr2(expr2.self()){}
+
+    double operator()(double x) const { 
+        return op(expr1(x), expr2(x)); 
+    }
+
+    private: 
+        const E1 expr1; 
+        const OP op; 
+        const E2 expr2; 
+}; 
+
+#endif
