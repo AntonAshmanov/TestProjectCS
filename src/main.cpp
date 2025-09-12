98a78dd521ae708a58a7844a127aa03730924ac1
AntonAshmanov <antosha.v.ashmanov@gmail.com>
2025-09-12 04:41:09 +0300

    add prev example + diagonal matrices

--- src/main.cpp
+++ src/main.cpp
@@ -26,9 +26,18 @@ int main() {
                           }
                         );                    
     
+    diag_matrix<csr> C({
+                            {1, 0, 0, 0, 0},
+                            {0, 2, 0, 0, 0},
+                            {0, 0, 3, 0, 0},
+                            {1, 0, 0, 4, 0},
+                            {0, 0, 0, 0, 5}
+                          }
+                        );                    
     print_matrix(A, A.get_size());
     print_matrix(2*A + B, A.get_size());
     print_matrix(2*A - 2*A, A.get_size());
+    print_matrix(B + C, B.get_size());
 
     return 0;
 }
